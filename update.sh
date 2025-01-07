7027aba305eb97e3d7af0816b3733c9396ef5da2
keggre <kegocandy@gmai.com>
2025-01-06 21:03:44 -0500

    Bugfix

--- update.sh
+++ update.sh
@@ -1,14 +1,14 @@
 #!/usr/bin/env sh
 
 if [ ! -n "${VERSION}" ]; then
-  VERSION=$(wget -O - https://api.github.com/repos/kegsmr/sc4mp-api/releases/latest | jq -r '.tag_name[1:]')
+  VERSION=$(wget -O - https://api.github.com/repos/kegsmr/sc4mp-api/releases/latest | jq -r '.tag_name')
 fi
 
-if [ ! -f "releases/v${VERSION}" ]; then
+if [ ! -f "releases/${VERSION}" ]; then
 
   rm -r ./releases && mkdir ./releases
-  wget -P releases https://github.com/kegsmr/sc4mp-api/archive/refs/tags/v$VERSION.zip
-  unzip ./releases/v$VERSION.zip -d ./src
+  wget -P releases https://github.com/kegsmr/sc4mp-api/archive/refs/tags/$VERSION.zip
+  unzip ./releases/$VERSION.zip -d ./src
 
   mv -f ./src/sc4mp-api-$VERSION/* .
   rm -r ./src
